2b2270a6b5c7905d04c7f142f5de38fa8e1302e3
kirill1347678@outlook.com <kirill1347678@outlook.com>
2024-10-03 22:00:15 +0300

    1

---
+++ StringTesting/Program.cs
@@ -0,0 +1,28 @@
+﻿using Testing;
+using static Testing.StrOperations;
+
+namespace StringTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StrOperations stringFunctions = new StrOperations();
+
+            string str1 = "Hello";
+            string str2 = "World";
+
+            string concatenated = stringFunctions.Concatenate(str1, str2);
+            string upper = stringFunctions.ToUpperCase(str1);
+            string lower = stringFunctions.ToLowerCase(str2);
+            bool contains = stringFunctions.ContainsSubstring(str1, "He");
+            string replaced = stringFunctions.ReplaceSubstring(str1, "He", "hi");
+
+            Console.WriteLine($"Concat: {concatenated}");
+            Console.WriteLine($"Upper case of '{str1}': {upper}");
+            Console.WriteLine($"Lower case of '{str2}': {lower}");
+            Console.WriteLine($"Does '{str1}' contain 'He'? {contains}");
+            Console.WriteLine($"Replace 'He' in '{str1}' with 'hi': {replaced}");
+        }
+    }
+}
---
+++ Testing/StrOperations.cs
@@ -0,0 +1,35 @@
+﻿namespace Testing
+{
+    public class StrOperations
+    {
+        // Конкатенация двух строк
+        public string Concatenate(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+
+        // Преобразование в верхний регистр
+        public string ToUpperCase(string input)
+        {
+            return input.ToUpper();
+        }
+
+        // Преобразование в нижний регистр
+        public string ToLowerCase(string input)
+        {
+            return input.ToLower();
+        }
+
+        // Проверка, содержит ли строка подстроку
+        public bool ContainsSubstring(string mainStr, string subStr)
+        {
+            return mainStr.Contains(subStr);
+        }
+
+        // Замена символов в строке
+        public string ReplaceSubstring(string input, string oldValue, string newValue)
+        {
+            return input.Replace(oldValue, newValue);
+        }
+    }
+}
